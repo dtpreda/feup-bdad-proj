f5e14dae53af8cb0c20d383084e8528ff9baad66
diogolhc <64315652+diogolhc@users.noreply.github.com>
2021-04-02 11:18:12 +0100

    Removes /extraSalary from HealthProfessional

--- src/criar.sql
+++ src/criar.sql
@@ -48,8 +48,7 @@ CREATE TABLE Patient (
 CREATE TABLE HealthProfessional (
     cc INTEGER PRIMARY KEY REFERENCES Person ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
     yearsOfService INTEGER CONSTRAINT yearsOfServiceRange CHECK(yearsOfService >= 0) DEFAULT 0 NOT NULL,
-    baseSalary INTEGER CONSTRAINT baseSalaryRange CHECK(baseSalary >= 665) DEFAULT 665 NOT NULL,
-    extraSalary INTEGER DEFAULT NULL
+    baseSalary INTEGER CONSTRAINT baseSalaryRange CHECK(baseSalary >= 665) DEFAULT 665 NOT NULL
 );
 
 CREATE TABLE EmployedAt (
