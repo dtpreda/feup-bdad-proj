4dd9b7e81ac03a413bc39bda223a8f0e1f3d1dd8
diogolhc <64315652+diogolhc@users.noreply.github.com>
2021-05-20 23:34:36 +0100

    int 4

--- src/ints/int4.sql
+++ src/ints/int4.sql
@@ -4,3 +4,11 @@
 
 -- Listar os chefes de unidades com o total pago a todos os HPs da sua unidade e contagem de HPs.
 
+--TODO isto agora faz a soma só dos salários base, depois é usar a view que vai ser criada para ter o salário completo
+
+
+SELECT head, SUM(baseSalary) AS totalPaid, COUNT(*) AS numberOfHealthProfessionals
+FROM Unit JOIN WorksAt
+ON (Unit.name=WorksAt.unitName AND Unit.hospital=WorksAt.hospitalName)
+JOIN HealthProfessional ON (HealthProfessional.cc=healthProfessional)
+GROUP BY head;
