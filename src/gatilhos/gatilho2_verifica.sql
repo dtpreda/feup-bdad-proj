64b6ffd11f04bb556ba8eadc8c1c507193222c29
up201905405 <up201905405@fe.up.pt>
2021-05-23 00:11:22 +0100

    gatilho2_verifica fixed

--- src/gatilhos/gatilho2_verifica.sql
+++ src/gatilhos/gatilho2_verifica.sql
@@ -2,13 +2,15 @@ PRAGMA foreign_keys=ON;
 .mode columns
 .headers ON
 
-INSERT INTO HealthProfessional (cc, yearsOfService, baseSalary) VALUES (88131651, 20, 5000);
+INSERT INTO Person (cc, name, birthDate) VALUES (46431142, 'Tim Jackson', '2001-08-21 11:21:33');
+INSERT INTO HealthProfessional (cc, yearsOfService, baseSalary) VALUES (46431142, 20, 5000);
+INSERT INTO Specialty (name, extraSalaryPerYear) VALUES ('Pediatrics', 559);
 
-SELECT * FROM HealthProfessional WHERE cc = 88131651; 
+SELECT * FROM HealthProfessional WHERE cc = 46431142; 
 
-INSERT INTO Doctor (healthProfessionalCC, type, specialty) VALUES (88131651, 'intern', 'Gynaecology');
+INSERT INTO Doctor (healthProfessionalCC, type, specialty) VALUES (46431142, 'intern', 'Pediatrics');
 
-SELECT * FROM HealthProfessional WHERE cc = 88131651; 
+SELECT * FROM HealthProfessional WHERE cc = 46431142; 
 
 SELECT cc, baseSalary, yearsOfService, specialty, extraSalaryPerYear, extraSalary
 FROM HealthProfessional 
